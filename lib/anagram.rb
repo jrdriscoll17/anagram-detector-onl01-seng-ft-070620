020d2f9146f3b8a920791eea0f8541216df34ba9
Jake Driscoll <jrdriscoll17@gmail.com>
2020-07-14 21:36:36 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,3 +1,16 @@
 class Anagram
+  attr_accessor :word, :word_list
 
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word, word_list)
+    matching_words = []
+    word_list.each do |word_from_list|
+      if word_from_list.split.sort == word.split.sort
+        matching_words << word_from_list
+      end
+    end
+  end
 end
