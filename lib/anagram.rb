3982040b3f8905aa9215a7f3595e462513efa846
Jake Driscoll <jrdriscoll17@gmail.com>
2020-07-14 21:40:13 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word, :word_list
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_list)
+    matching_words = []
+    word_list.each do |word_from_list|
+      if word_from_list.split("").sort == @word.split("").sort
+        matching_words << word_from_list
+      end
+    end
+    matching_words
+  end
+end
